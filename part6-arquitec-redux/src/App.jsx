be471a681a8d9f4d1f5bcbd2be3417cc3b9edf27
Nicobrandoni98 <nico.brandoni98@gmail.com>
2024-10-31 15:30:06 -0300

    part6 cap C

--- part6-arquitec-redux/src/App.jsx
+++ part6-arquitec-redux/src/App.jsx
@@ -1,13 +1,18 @@
-import Notes from "./components/Notes"
-import NewNote from "./components/NewNote"
+import Notes from "./components/Notes";
+import NewNote from "./components/NewNote";
+import VisibilityFilter  from "./components/visibilityFilter";
 
 const App = () => {
-  return(
+  const filterSelected = (value) => {
+    console.log(value);
+  };
+  return (
     <div>
       <NewNote />
+      <VisibilityFilter />
       <Notes />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;

3162622ea0b8d91fa614dabc08782f7dfd695c48
Nicobrandoni98 <nico.brandoni98@gmail.com>
2024-11-27 16:03:57 -0300

    part 7 transpiladores

--- 7part/src/App.jsx
+++ 7part/src/App.jsx
@@ -7,10 +7,48 @@ import {
   Route,
   Link,
   Navigate,
-  useParams,
   useNavigate,
   useMatch,
 } from "react-router-dom";
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableRow,
+  Paper
+} from "@mui/material";
+
+import styled from "styled-components";
+
+const Button = styled.button`
+  background: Bisque;
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid Chocolate;
+  border-radius: 3px;
+`;
+
+const Input = styled.input`
+  margin: 0.25em;
+`;
+const Page = styled.div`
+  padding: 1em;
+  background: papayawhip;
+`;
+
+const Navigation = styled.div`
+  background: BurlyWood;
+  padding: 1em;
+`;
+
+const Footer = styled.div`
+  background: Chocolate;
+  padding: 1em;
+  margin-top: 1em;
+`;
 
 const Home = () => (
   <div>
@@ -34,21 +72,31 @@ const Note = ({ note }) => {
     <div>
       <h2>{note.content}</h2>
       <div>{note.user}</div>
-      <div><strong>{note.important ? 'important' : ''}</strong></div>
+      <div>
+        <strong>{note.important ? "important" : ""}</strong>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 const Notes = ({ notes }) => (
   <div>
     <h2>Notes</h2>
-    <ul>
-      {notes.map((note) => (
-        <li key={note.id}>
-          <Link to={`/notes/${note.id}`}>{note.content}</Link>
-        </li>
-      ))}
-    </ul>
+
+    <TableContainer component={Paper}>
+      <Table>
+        <TableBody>
+          {notes.map((note) => (
+            <TableRow key={note.id}>
+              <TableCell>
+                <Link to={`/notes/${note.id}`}>{note.content}</Link>
+              </TableCell>
+              <TableCell>{note.user}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   </div>
 );
 
@@ -68,7 +116,7 @@ const Login = (props) => {
 
   const onSubmit = (event) => {
     event.preventDefault();
-    props.onLogin("Nico");
+    props.onLogin("Nicolas");
     navigate("/");
   };
 
@@ -77,12 +125,16 @@ const Login = (props) => {
       <h2>login</h2>
       <form onSubmit={onSubmit}>
         <div>
-          username: <input />
+          username:
+          <Input />
         </div>
         <div>
-          password: <input type="password" />
+          password:
+          <Input type="password" />
         </div>
-        <button type="submit">login</button>
+        <Button type="submit" primary="">
+          login
+        </Button>
       </form>
     </div>
   );
@@ -111,9 +163,14 @@ const App = () => {
   ]);
 
   const [user, setUser] = useState(null);
+  const [message, setMessage] = useState(null);
 
   const login = (user) => {
     setUser(user);
+    setMessage(`welcome ${user}`);
+    setTimeout(() => {
+      setMessage(null);
+    }, 2000);
   };
 
   const padding = {
@@ -125,43 +182,31 @@ const App = () => {
     ? notes.find((note) => note.id === Number(match.params.id))
     : null;
 
-  return (
-    <div>
-      <div>
-        <Link style={padding} to="/">
-          home
-        </Link>
-        <Link style={padding} to="/notes">
-          notes
-        </Link>
-        <Link style={padding} to="/users">
-          users
-        </Link>
-        {user ? (
-          <em>{user} logged in</em>
-        ) : (
-          <Link style={padding} to="/login">
-            login
-          </Link>
-        )}
-      </div>
-
-      <Routes>
-        <Route path="/notes/:id" element={<Note note={note} />} />
-        <Route path="/notes" element={<Notes notes={notes} />} />
-        <Route
-          path="/users"
-          element={user ? <Users /> : <Navigate replace to="/login" />}
-        />
-        <Route path="/login" element={<Login onLogin={login} />} />
-        <Route path="/" element={<Home />} />
-      </Routes>
-      <div>
-        <br />
-        <em>Note app, Department of Computer Science 2023</em>
-      </div>
-    </div>
-  );
+    return (
+      <Page>
+       <Navigation>
+         <Link style={padding} to="/">home</Link>
+         <Link style={padding} to="/notes">notes</Link>
+         <Link style={padding} to="/users">users</Link>
+         {user
+           ? <em>{user} logged in</em>
+           : <Link style={padding} to="/login">login</Link>
+         }
+       </Navigation>
+       
+       <Routes>
+         <Route path="/notes/:id" element={<Note note={note} />} />  
+         <Route path="/notes" element={<Notes notes={notes} />} />   
+         <Route path="/users" element={user ? <Users /> : <Navigate replace to="/login" />} />
+         <Route path="/login" element={<Login onLogin={login} />} />
+         <Route path="/" element={<Home />} />      
+       </Routes>
+ 
+       <Footer>
+         <em>Note app, Department of Computer Science 2022</em>
+       </Footer>
+     </Page>
+   )
 };
 
 ReactDOM.createRoot(document.getElementById("root")).render(
---
+++ part7.version2/src/App.js
@@ -0,0 +1,6 @@
+const App = () =>
+    react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(
+      'div',
+      null,
+      'hello webpack'
+    )
